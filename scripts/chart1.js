d3f89671d96728ee21bb956e0edceef1423d1e47
infograafikko <joel@infograafikko.fi>
2016-12-24 12:10:54 +0200

    Add files via upload

--- scripts/chart1.js
+++ scripts/chart1.js
@@ -87,9 +87,18 @@ function ready (error, data) {
             div.transition()		
                 .duration(200)		
                 .style("opacity", .99);
-            div.html( "<strong> Kunta tai kaupunki: </strong>"+ d["Kunta tai kaupunki"] + "<br /><strong>Päivämäärä:</strong> " + d["Päivämäärä"] + "<br /><strong>Havainnon kuvaus:</strong><br />" + d["kuvaus"] + " <a href=" + d["URL"] + "target='_blank'>Lue lisää...</a>" )	
+            if (d["havaintokuvat"] == "Kyllä") {
+            div.html( "<strong> Kunta tai kaupunki: </strong>"+ d["Kunta tai kaupunki"] + "<br /><strong>Päivämäärä:</strong> " + d["Päivämäärä"] + "<br /><strong>Havaintokuva:</strong><br /><img src=" + d["kuva1"] +">")	
                 .style("left", (d3.event.pageX) + "px")		
                 .style("top", (d3.event.pageY - 28) + "px");	
+            }
+
+            else {
+            div.html( "<strong> Kunta tai kaupunki: </strong>"+ d["Kunta tai kaupunki"] + "<br /><strong>Päivämäärä:</strong> " + d["Päivämäärä"] + "<br /><strong>Havainnon kuvaus:</strong><br />" + d["kuvaus"] )	
+                .style("left", (d3.event.pageX) + "px")		
+                .style("top", (d3.event.pageY - 28) + "px");	
+            }
+
             })					
         .on("mouseout", function(d) {		
             div.transition()		
@@ -455,32 +464,7 @@ function fourthChart(error, data) {
 
 		})
 
-	//If havaintokuvia
 
-	circles = svg.selectAll(".havainnot")
-		.on("mouseover", function(d) {		
-            div.transition()		
-                .duration(200)		
-                .style("opacity", .99);
-
-            if (d["havaintokuvat"] == "Kyllä") {
-            div.html( "<strong> Kunta tai kaupunki: </strong>"+ d["Kunta tai kaupunki"] + "<br /><strong>Päivämäärä:</strong> " + d["Päivämäärä"] + "<br /><strong>Havaintokuva:</strong><br /><img src=" + d["kuva1"] +">" + " <a href=" + d["URL"] + "target='_blank'>Lue lisää...</a>")	
-                .style("left", (d3.event.pageX) + "px")		
-                .style("top", (d3.event.pageY - 28) + "px");	
-            }
-
-            else {
-            div.html( "<strong> Kunta tai kaupunki: </strong>"+ d["Kunta tai kaupunki"] + "<br /><strong>Päivämäärä:</strong> " + d["Päivämäärä"] + "<br /><strong>Havainnon kuvaus:</strong><br />" + d["kuvaus"] + " <a href=" + d["URL"] + "target='_blank'>Lue lisää...</a>" )	
-                .style("left", (d3.event.pageX) + "px")		
-                .style("top", (d3.event.pageY - 28) + "px");	
-            }
-
-            })					
-        .on("mouseout", function(d) {		
-            div.transition()		
-                .duration(500)		
-                .style("opacity", 0);	
-        })
 
 
 
